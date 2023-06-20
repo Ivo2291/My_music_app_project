fad58982303e0ea0d42c0c98563042edf5b8b7eb
ivo2291 <ivo2291@gmail.com>
2023-06-20 16:18:20 +0300

    Modifying templates and forms

--- My_music_app_project/my_music_app/forms.py
+++ My_music_app_project/my_music_app/forms.py
@@ -2,13 +2,34 @@ from django import forms
 from My_music_app_project.my_music_app.models import Profile, Album
 
 
-class ProfileCreateForm(forms.ModelForm):
+class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = '__all__'
 
 
-class AlbumCreateForm(forms.ModelForm):
+class ProfileCreateForm(ProfileForm):
+    pass
+
+
+class ProfileDeleteForm(ProfileForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__set_fields_to_hidden()
+
+    def save(self, commit=True):
+        if commit:
+            self.instance.delete()
+            Album.objects.all().delete()
+        else:
+            return self.instance
+
+    def __set_fields_to_hidden(self):
+        for _, field in self.fields.items():
+            field.widget = forms.HiddenInput()
+
+
+class AlbumForm(forms.ModelForm):
     class Meta:
         model = Album
         fields = '__all__'
@@ -44,3 +65,27 @@ class AlbumCreateForm(forms.ModelForm):
                 }
             ),
         }
+
+
+class AlbumCreateForm(AlbumForm):
+    pass
+
+
+class AlbumEditForm(AlbumForm):
+    pass
+
+
+class AlbumDeleteForm(AlbumForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__set_fields_to_disable()
+
+    def save(self, commit=True):
+        if commit:
+            self.instance.delete()
+        else:
+            return self.instance
+
+    def __set_fields_to_disable(self):
+        for _, field in self.fields.items():
+            field.widget.attrs['readonly'] = True
--- My_music_app_project/my_music_app/views.py
+++ My_music_app_project/my_music_app/views.py
@@ -1,4 +1,5 @@
-from My_music_app_project.my_music_app.forms import ProfileCreateForm, AlbumCreateForm
+from My_music_app_project.my_music_app.forms import ProfileCreateForm, AlbumCreateForm, AlbumEditForm, AlbumDeleteForm, \
+    ProfileDeleteForm
 from My_music_app_project.my_music_app.models import Profile, Album
 from django.shortcuts import render, redirect
 
@@ -25,7 +26,6 @@ def index(request):
 
 
 def details_album_page(request, pk):
-
     album = Album.objects.filter(pk=pk).get()
 
     context = {
@@ -52,7 +52,16 @@ def add_album_page(request):
 def edit_album_page(request, pk):
     album = Album.objects.filter(pk=pk).get()
 
+    if request.method == 'GET':
+        form = AlbumEditForm(instance=album)
+    else:
+        form = AlbumEditForm(request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
     context = {
+        'form': form,
         'album': album,
     }
 
@@ -60,11 +69,34 @@ def edit_album_page(request, pk):
 
 
 def delete_album_page(request, pk):
-    return render(request, 'albums/delete-album.html')
+    album = Album.objects.filter(pk=pk).get()
+
+    if request.method == 'GET':
+        form = AlbumDeleteForm(instance=album)
+    else:
+        form = AlbumDeleteForm(request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {
+        'form': form,
+        'album': album,
+    }
+
+    return render(request, 'albums/delete-album.html', context)
 
 
 def details_profile_page(request):
-    return render(request, 'profiles/profile-details.html')
+    profile = get_profile()
+    albums_count = Album.objects.count()
+
+    context = {
+        'profile': profile,
+        'albums_count': albums_count
+    }
+
+    return render(request, 'profiles/profile-details.html', context)
 
 
 def add_profile_page(request):
@@ -82,10 +114,24 @@ def add_profile_page(request):
 
     context = {
         'form': form,
+        'hide_buttons': True,
     }
 
     return render(request, 'core/home-no-profile.html', context)
 
 
 def delete_profile_page(request):
-    return render(request, 'profiles/profile-delete.html')
+    profile = get_profile()
+
+    if request.method == "GET":
+        form = ProfileDeleteForm(instance=profile)
+    else:
+        form = ProfileDeleteForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'profiles/profile-delete.html', context)
